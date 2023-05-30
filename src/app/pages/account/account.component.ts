0cecc74214cb71053a269c421f2c16c18cbe8239
Joao Pimenta Giudice <58566178+Azurba@users.noreply.github.com>
2023-05-30 08:53:12 -0600

    add fix on btns

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { OrderHistory } from 'src/app/models/orderHistory';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
@@ -13,7 +14,7 @@ export class AccountComponent {
   user? : User;
   orderHistory : OrderHistory[] = [];
 
-  constructor(private userService : UserService){
+  constructor(private userService : UserService, private route : Router){
     console.log()
   }
 
@@ -44,5 +45,11 @@ export class AccountComponent {
         console.error('Failed to retrieve order history:', error);
       }
     );
+  }
+
+  logout(){
+    this.route.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.route.navigate([this.route.url]);
+    });
   }
 }
